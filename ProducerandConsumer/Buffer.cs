748fe6a8ffa5c7aae13f829ccaa5dec7d7ce8fbd
ivanrivas <ivan.rivas@cetys.edu.mx>
2013-10-02 01:22:36 -0700

    ProducerAndConsumer

---
+++ ProducerandConsumer/Buffer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ProducerandConsumer
+{
+    public class Buffer
+    {
+         Semaphore semvacio;
+         Semaphore semnolleno;
+         int[] buf;
+
+         int producer, consumer, size, numProcesos;
+
+        public Buffer(int tam)
+        {
+            buf = new int[tam];
+            semvacio = new Semaphore(0, tam);
+            semnolleno = new Semaphore(tam, tam);
+            consumer = 0;
+            producer = 0;
+            size = tam;
+            numProcesos = 0;
+        }
+        public void Producer(int proceso)
+        {
+            semnolleno.WaitOne();
+                buf[producer++] = proceso;
+                if (producer == size)
+                {
+                    producer = 0;
+                }
+                numProcesos++;
+            semvacio.Release();
+        }
+        public int Consumer()
+        {
+            int proceso;
+           semvacio.WaitOne();
+                proceso = buf[consumer++];
+                if (consumer == size)
+                {
+                    consumer = 0;
+                }
+                numProcesos--;
+            semnolleno.Release();
+            return proceso;
+        }
+
+    }
+}
---
+++ ProducerandConsumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ProducerandConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Buffer buf = new Buffer(1);
+            Random ran = new Random();
+            var p1 = new Thread(() =>
+                {
+
+                    while (true)
+                    {
+                        int p = ran.Next(1, 100);
+                        buf.Producer(p);
+                        Console.WriteLine("Se ha producido el proceso # " + p + " de P1 \n");
+                        Thread.Sleep(1000);
+                    }
+                });
+            var c1 = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        int c = buf.Consumer();
+                        Console.WriteLine("Se ha consumido el proceso # " + c + " de P1" + " \n");
+                        Thread.Sleep(1000);
+                    }
+                });
+
+
+            p1.Start();
+            c1.Start();
+            Console.ReadLine();
+        }
+    }
+}
